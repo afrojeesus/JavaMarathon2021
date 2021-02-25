18eb5fd5c1e98adfbae1ef9059f36fa1a1aa4136
Maxim Simonenko <maksim.simonenko@gmail.com>
2021-02-25 22:29:07 +0300

    Java Marathon Day3 complete

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,28 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        while (true) {
+
+            System.out.println("Введите делимое: ");
+            double divident = input.nextDouble();
+
+            System.out.println("Введите делитель: ");
+            double divider = input.nextDouble();
+            
+            if (divider == 0) {
+                break;
+            }
+            System.out.println(divident / divider);
+
+        }
+        System.out.println("Работа программы завершена");
+
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,33 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double divident;
+        double divider;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Введите делимое: ");
+            divident = input.nextDouble();
+
+            System.out.println("Введите делитель: ");
+            divider = input.nextDouble();
+
+            if (divider == 0) {
+                System.out.println("Деление на ноль!");
+                continue;
+            }
+            System.out.println(divident / divider);
+            System.out.println("Счетчик = " + (i + 1));
+
+        }
+        System.out.println("Программа завершила свою работу");
+
 
     }
 }
+
+
+
