24e848166d94373d1c7ac28199761405bd927f01
Maxim Simonenko <maksim.simonenko@gmail.com>
2021-03-17 14:29:32 +0300

    Java Marathon Day 8 complete

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,26 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        long start1 = System.currentTimeMillis();
+        String str1 = new String();
+        for (int i = 0; i <= 20000; i++) {
+            str1 += i + " ";
+        }
+        long finish1 = System.currentTimeMillis();
+        System.out.println(str1);
+        System.out.println(finish1 - start1);
+
+        StringBuilder str2 = new StringBuilder();
+
+        long start2 = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            str2.append(i + " ");
+        }
+        long finish2 = System.currentTimeMillis();
+
+        System.out.println(str2);
+        System.out.println(finish2 - start2);
+
+
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane plane = new Airplane("Boeing", 1234123, 12341234, 12341234);
+        System.out.println(plane);
 
     }
 }
